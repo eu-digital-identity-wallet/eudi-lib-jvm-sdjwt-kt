4f36f0b80481adfb103c37c26acc9159116c095b
akaklis <antonis.kaklis@netcompany-intrasoft.com>
2023-07-07 10:58:18 +0300

    Update SdJwtIssuanceVerifierTest.kt
    Added test cases for the sd-jwt verification - Issuance scenario

--- src/test/kotlin/eu/europa/ec/eudi/sdjwt/SdJwtIssuanceVerifierTest.kt
+++ src/test/kotlin/eu/europa/ec/eudi/sdjwt/SdJwtIssuanceVerifierTest.kt
@@ -16,6 +16,8 @@
 package eu.europa.ec.eudi.sdjwt
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class SdJwtIssuanceVerifierTest {
@@ -49,4 +51,129 @@ class SdJwtIssuanceVerifierTest {
             Jsb2NhbGl0eSI6ICJTY2h1bHBmb3J0YSIsICJyZWdpb24iOiAiU2FjaHNlbi1BbmhhbH
             QiLCAiY291bnRyeSI6ICJERSJ9XQ
     """.trimIndent().removeNewLine()
+
+    private fun verifyIssuanceExpectingError(
+        expectedError: VerificationError,
+        jwtSignatureVerifier: JwtSignatureVerifier,
+        //holderBindingVerifier: HolderBindingVerifier,
+        unverifiedSdJwt: String,
+    ) {
+        val verification = SdJwtVerifier.verifyIssuance(
+            jwtSignatureVerifier = jwtSignatureVerifier,
+            unverifiedSdJwt = unverifiedSdJwt,
+        )
+        verification.fold(
+            onSuccess = { fail("Was expecting error") },
+            onFailure = { exception ->
+                if (exception is SdJwtVerificationException) {
+                    assertEquals(expectedError, exception.reason)
+                } else {
+                    fail(exception)
+                }
+            },
+        )
+    }
+    @Test
+    fun `when sd-jwt is empty verify should return ParsingError`() {
+        verifyIssuanceExpectingError(
+            VerificationError.ParsingError,
+            JwtSignatureVerifier.NoSignatureValidation,
+            "",
+        )
+    }
+
+    @Test
+    fun `when sd-jwt has an invalid jwt but no disclosures , no ending ~ verify should return ParsingError`() {
+        verifyIssuanceExpectingError(
+            VerificationError.ParsingError,
+            JwtSignatureVerifier.NoSignatureValidation,
+            "jwt",
+        )
+    }
+
+    @Test
+    fun `when sd-jwt has a valid jwt, no disclosures and no holderBinding, no ending ~ verify should return ParsingError`() {
+        verifyIssuanceExpectingError(
+            VerificationError.ParsingError,
+            JwtSignatureVerifier.NoSignatureValidation,
+            jwt,
+        )
+    }
+
+    @Test
+    fun `when sd-jwt has an invalid jwt but no disclosures verify should return InvalidJwt`() {
+        verifyIssuanceExpectingError(
+            VerificationError.InvalidJwt,
+            JwtSignatureVerifier.NoSignatureValidation,
+            "jwt~",
+        )
+    }
+
+    @Test
+    fun `when sd-jwt has an invalid jwt, no disclosures and has holderBinding verify should return InvalidJwt`() {
+        verifyIssuanceExpectingError(
+            VerificationError.InvalidJwt,
+            JwtSignatureVerifier.NoSignatureValidation,
+            "jwt~hb",
+        )
+    }
+
+    @Test
+    fun `when sd-jwt has a valid jwt, no disclosures and no holderBinding verify should return Valid`() {
+        verifyIssuanceSuccess(
+            JwtSignatureVerifier.NoSignatureValidation,
+            "$jwt~",
+        )
+    }
+
+    @Test
+    fun `when sd-jwt has an valid jwt, no disclosures and invalid holderBinding verify should return InvalidHolderBindingJwt`() {
+        verifyIssuanceExpectingError(
+            VerificationError.HolderBindingFailed(HolderBindingError.InvalidHolderBindingJwt),
+            JwtSignatureVerifier.NoSignatureValidation,
+            "$jwt~hb",
+        )
+    }
+
+    @Test
+    fun `when sd-jwt has an valid jwt, no disclosures and valid holderBinding verify should return Valid`() {
+        verifyIssuanceSuccess(
+            JwtSignatureVerifier.NoSignatureValidation,
+            "$jwt~$jwt",
+        )
+    }
+
+    @Test
+    fun `when sd-jwt has an valid jwt, invalid disclosures verify should return InvalidDisclosures`() {
+        verifyIssuanceExpectingError(
+            VerificationError.InvalidDisclosures(listOf("d1", "d2")),
+            JwtSignatureVerifier.NoSignatureValidation,
+            "$jwt~d1~d2~",
+        )
+    }
+
+    @Test
+    fun `when sd-jwt has an valid jwt, valid disclosures verify should return Valid`() {
+        verifyIssuanceSuccess(
+            JwtSignatureVerifier.NoSignatureValidation,
+            "$jwt~$d1~",
+        )
+    }
+
+    @Test
+    fun `when sd-jwt has an valid jwt, non unique disclosures verify should return NonUnqueDisclosures`() {
+        verifyIssuanceExpectingError(
+            VerificationError.NonUnqueDisclosures,
+            JwtSignatureVerifier.NoSignatureValidation,
+            "$jwt~$d1~$d1~",
+        )
+    }
+
+    @Test
+    fun `when sd-jwt has an valid jwt, valid disclosures and valid holder binding verify should return Valid`() {
+        verifyIssuanceSuccess(
+            JwtSignatureVerifier.NoSignatureValidation,
+            "$jwt~$d1~$jwt",
+        )
+    }
 }
