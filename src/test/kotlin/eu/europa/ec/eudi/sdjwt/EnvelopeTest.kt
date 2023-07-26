deb4b15e42c68ab73ae8aed73d775d49c2c07402
Routis Babis <haralampos.routis@netcompany-intrasoft.com>
2023-07-26 23:31:21 +0300

    Added EnvelopeTest

---
+++ src/test/kotlin/eu/europa/ec/eudi/sdjwt/EnvelopeTest.kt
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2023 European Commission
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.europa.ec.eudi.sdjwt
+
+import com.nimbusds.jose.JWSAlgorithm
+import com.nimbusds.jose.jwk.Curve
+import com.nimbusds.jose.jwk.ECKey
+import com.nimbusds.jose.jwk.KeyUse
+import com.nimbusds.jose.jwk.gen.ECKeyGenerator
+import java.time.Instant
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class EnvelopeTest {
+
+    @Test
+    fun `given an SD-JWT make sure that envelope JWT contains the expected claims`() {
+        //
+        // given
+        //
+        val iat = Instant.now()
+        val nonce = "yolo"
+        val aud = "MeMySelfAndI"
+
+        val envelopClaims = sdJwt.toEnvelopedFormat(
+            issuedAt = iat,
+            nonce = nonce,
+            audience = aud,
+            serializeJwt = { it },
+            signingKey = key(),
+            signAlgorithm = JWSAlgorithm.ES384,
+        ).getOrThrow().jwtClaimsSet
+
+        //
+        // then
+        //
+        assertEquals(iat.epochSecond, envelopClaims.issueTime.toInstant().epochSecond)
+        assertEquals(nonce, envelopClaims.getStringClaim("nonce"))
+        assertEquals(aud, envelopClaims.audience.first())
+        assertEquals(unverifiedSdJwt, envelopClaims.getStringClaim("_sd_jwt"))
+    }
+
+    val sdJwt: SdJwt.Presentation<Jwt, Nothing> by lazy {
+        val sdJwt = SdJwtVerifier.verifyPresentation(
+            jwtSignatureVerifier = JwtSignatureVerifier.NoSignatureValidation,
+            keyBindingVerifier = KeyBindingVerifier.MustNotBePresent,
+            unverifiedSdJwt = unverifiedSdJwt,
+        ).getOrThrow()
+        SdJwt.Presentation(sdJwt.jwt.first, sdJwt.disclosures, null)
+    }
+
+    fun key(): ECKey =
+        ECKeyGenerator(Curve.P_384)
+            .keyID("id")
+            .keyUse(KeyUse.SIGNATURE)
+            .generate()
+
+    val unverifiedSdJwt = """
+            eyJhbGciOiAiRVMyNTYifQ.eyJfc2QiOiBbIkNyUWU3UzVrcUJBSHQtbk1ZWGdjNmJkd
+            DJTSDVhVFkxc1VfTS1QZ2tqUEkiLCAiSnpZakg0c3ZsaUgwUjNQeUVNZmVadTZKdDY5d
+            TVxZWhabzdGN0VQWWxTRSIsICJQb3JGYnBLdVZ1Nnh5bUphZ3ZrRnNGWEFiUm9jMkpHb
+            EFVQTJCQTRvN2NJIiwgIlRHZjRvTGJnd2Q1SlFhSHlLVlFaVTlVZEdFMHc1cnREc3Jae
+            mZVYW9tTG8iLCAiWFFfM2tQS3QxWHlYN0tBTmtxVlI2eVoyVmE1TnJQSXZQWWJ5TXZSS
+            0JNTSIsICJYekZyendzY002R242Q0pEYzZ2Vks4QmtNbmZHOHZPU0tmcFBJWmRBZmRFI
+            iwgImdiT3NJNEVkcTJ4Mkt3LXc1d1BFemFrb2I5aFYxY1JEMEFUTjNvUUw5Sk0iLCAia
+            nN1OXlWdWx3UVFsaEZsTV8zSmx6TWFTRnpnbGhRRzBEcGZheVF3TFVLNCJdLCAiaXNzI
+            jogImh0dHBzOi8vZXhhbXBsZS5jb20vaXNzdWVyIiwgImlhdCI6IDE2ODMwMDAwMDAsI
+            CJleHAiOiAxODgzMDAwMDAwLCAic3ViIjogInVzZXJfNDIiLCAibmF0aW9uYWxpdGllc
+            yI6IFt7Ii4uLiI6ICJwRm5kamtaX1ZDem15VGE2VWpsWm8zZGgta284YUlLUWM5RGxHe
+            mhhVllvIn0sIHsiLi4uIjogIjdDZjZKa1B1ZHJ5M2xjYndIZ2VaOGtoQXYxVTFPU2xlc
+            lAwVmtCSnJXWjAifV0sICJfc2RfYWxnIjogInNoYS0yNTYiLCAiY25mIjogeyJqd2siO
+            iB7Imt0eSI6ICJFQyIsICJjcnYiOiAiUC0yNTYiLCAieCI6ICJUQ0FFUjE5WnZ1M09IR
+            jRqNFc0dmZTVm9ISVAxSUxpbERsczd2Q2VHZW1jIiwgInkiOiAiWnhqaVdXYlpNUUdIV
+            ldLVlE0aGJTSWlyc1ZmdWVjQ0U2dDRqVDlGMkhaUSJ9fX0.kmx687kUBiIDvKWgo2Dub
+            -TpdCCRLZwtD7TOj4RoLsUbtFBI8sMrtH2BejXtm_P6fOAjKAVc_7LRNJFgm3PJhg~Wy
+            IyR0xDNDJzS1F2ZUNmR2ZyeU5STjl3IiwgImdpdmVuX25hbWUiLCAiSm9obiJd~WyJlb
+            HVWNU9nM2dTTklJOEVZbnN4QV9BIiwgImZhbWlseV9uYW1lIiwgIkRvZSJd~WyI2SWo3
+            dE0tYTVpVlBHYm9TNXRtdlZBIiwgImVtYWlsIiwgImpvaG5kb2VAZXhhbXBsZS5jb20i
+            XQ~WyJlSThaV205UW5LUHBOUGVOZW5IZGhRIiwgInBob25lX251bWJlciIsICIrMS0yM
+            DItNTU1LTAxMDEiXQ~WyJRZ19PNjR6cUF4ZTQxMmExMDhpcm9BIiwgInBob25lX251bW
+            Jlcl92ZXJpZmllZCIsIHRydWVd~WyJBSngtMDk1VlBycFR0TjRRTU9xUk9BIiwgImFkZ
+            HJlc3MiLCB7InN0cmVldF9hZGRyZXNzIjogIjEyMyBNYWluIFN0IiwgImxvY2FsaXR5I
+            jogIkFueXRvd24iLCAicmVnaW9uIjogIkFueXN0YXRlIiwgImNvdW50cnkiOiAiVVMif
+            V0~WyJQYzMzSk0yTGNoY1VfbEhnZ3ZfdWZRIiwgImJpcnRoZGF0ZSIsICIxOTQwLTAxL
+            TAxIl0~WyJHMDJOU3JRZmpGWFE3SW8wOXN5YWpBIiwgInVwZGF0ZWRfYXQiLCAxNTcwM
+            DAwMDAwXQ~WyJsa2x4RjVqTVlsR1RQVW92TU5JdkNBIiwgIlVTIl0~WyJuUHVvUW5rUk
+            ZxM0JJZUFtN0FuWEZBIiwgIkRFIl0~
+    """.trimIndent().removeNewLine()
+}
