f8ff37370037603548add4890ad4539f2f7f2a73
Routis Babis <haralampos.routis@netcompany-intrasoft.com>
2023-09-05 18:23:38 +0300

    Added VC DM mapping

--- src/test/kotlin/eu/europa/ec/eudi/sdjwt/Vc.kt
+++ src/test/kotlin/eu/europa/ec/eudi/sdjwt/Vc.kt
@@ -19,9 +19,11 @@ package eu.europa.ec.eudi.sdjwt
 // import com.danubetech.verifiablecredentials.VerifiableCredential
 // import com.danubetech.verifiablecredentials.jsonld.VerifiableCredentialContexts
 // import foundation.identity.jsonld.JsonLDUtils
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.*
 import java.net.URI
 import java.time.Instant
+import java.time.format.DateTimeFormatter
 
 typealias JsonLdContext = URI
 typealias Id = URI
@@ -32,30 +34,67 @@ data class CredentialSubject<out CLAIMS>(
     val id: Id,
     val claims: CLAIMS,
 )
+
 data class VerifiableCredential<out CLAIMS>(
     val context: List<JsonLdContext>,
-    val type: Type,
+    val type: List<Type>,
     val id: Id,
     val issuer: Issuer,
     val issuanceDate: Instant,
-    val expirationDate: Instant,
+    val expirationDate: Instant?,
     val credentialSubject: CredentialSubject<CLAIMS>,
 )
 
-fun <CLAIMS>jwtClaimSetOf(
+fun main() {
+    val vc = VerifiableCredential<JsonObject>(
+        context = listOf(
+            "https://www.w3.org/2018/credentials/v1",
+            "https://www.w3.org/2018/credentials/examples/v1",
+        ).map { URI.create(it) },
+        id = URI.create("http://example.com/credentials/4643"),
+        type = listOf("VerifiableCredential"),
+        issuer = URI.create("https://example.com/issuers/14"),
+        issuanceDate = Instant.parse("2018-02-24T05:28:04Z"),
+        expirationDate = null,
+        credentialSubject = CredentialSubject(
+            id = URI.create("did:example:abcdef1234567"),
+            claims = buildJsonObject {
+                put("name", "Jane Doe")
+            },
+        ),
+    )
+    val jwtCS = jwtClaimSetOf(vc, listOf("aud"), true) { it }
+    println(json.encodeToString(jwtCS))
+}
+
+fun <CLAIMS> jwtClaimSetOf(
     vc: VerifiableCredential<CLAIMS>,
-    f: (CLAIMS) -> JsonElement,
-): Pair<JsonObject, JsonObject> {
-    val jwsHeader = buildJsonObject {
-        put("typ", "JWT")
-    }
-
-    val jwtClaimSet = buildJsonObject {
-        exp(vc.expirationDate.epochSecond)
+    aud: List<String>,
+    duplicate: Boolean = false,
+    f: (CLAIMS) -> JsonObject,
+): JsonObject {
+    return buildJsonObject {
+        vc.expirationDate?.let { exp(it.epochSecond) }
         iss(vc.issuer.toString())
-
+        nbf(vc.issuanceDate.epochSecond)
+        jti(vc.id.toString())
+        sub(vc.credentialSubject.id.toString())
+        aud(*aud.toTypedArray())
         putJsonObject("vc") {
+            if (duplicate) {
+                put("id", vc.id.toString())
+                vc.expirationDate?.let {
+                    put("expirationDate", DateTimeFormatter.ISO_DATE_TIME.format(it))
+                }
+
+                put("issuer", vc.issuer.toString())
+                put("issuanceDate", DateTimeFormatter.ISO_ZONED_DATE_TIME.format(vc.issuanceDate))
+            }
+            val cs = f(vc.credentialSubject.claims).toMutableMap()
+            if (duplicate) {
+                cs["id"] = JsonPrimitive(vc.credentialSubject.id.toString())
+            }
+            put("credentialSubject", JsonObject(cs))
         }
     }
-    return jwsHeader to jwtClaimSet
 }
