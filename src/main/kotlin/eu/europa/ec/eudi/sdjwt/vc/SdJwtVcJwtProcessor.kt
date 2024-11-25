ccc270d12c908c97d5b2ef04d71fd26d22c7b919
babisRoutis <haralampos.routis@netcompany.com>
2024-11-25 15:33:22 +0200

    Introduced Procecssor

--- src/main/kotlin/eu/europa/ec/eudi/sdjwt/vc/SdJwtVcJwtProcessor.kt
+++ src/main/kotlin/eu/europa/ec/eudi/sdjwt/vc/SdJwtVcJwtProcessor.kt
@@ -19,25 +19,20 @@ import com.nimbusds.jose.JOSEObjectType
 import com.nimbusds.jose.JWSAlgorithm
 import com.nimbusds.jose.JWSHeader
 import com.nimbusds.jose.JWSVerifier
-import com.nimbusds.jose.crypto.ECDSAVerifier
-import com.nimbusds.jose.crypto.Ed25519Verifier
-import com.nimbusds.jose.crypto.MACVerifier
-import com.nimbusds.jose.crypto.RSASSAVerifier
 import com.nimbusds.jose.crypto.factories.DefaultJWSVerifierFactory
 import com.nimbusds.jose.jwk.*
 import com.nimbusds.jose.jwk.source.JWKSource
-import com.nimbusds.jose.proc.BadJOSEException
 import com.nimbusds.jose.proc.DefaultJOSEObjectTypeVerifier
+import com.nimbusds.jose.proc.JOSEObjectTypeVerifier
 import com.nimbusds.jose.proc.JWSVerificationKeySelector
 import com.nimbusds.jose.proc.SecurityContext
 import com.nimbusds.jwt.JWTClaimsSet
-import com.nimbusds.jwt.SignedJWT
-import com.nimbusds.jwt.proc.BadJWTException
 import com.nimbusds.jwt.proc.DefaultJWTClaimsVerifier
 import com.nimbusds.jwt.proc.DefaultJWTProcessor
+import com.nimbusds.jwt.proc.JWTClaimsSetVerifier
 import com.nimbusds.jwt.proc.JWTProcessor
+import eu.europa.ec.eudi.sdjwt.JwkSourceJWTProcessor
 import java.security.Key
-import java.text.ParseException
 
 const val SD_JWT_VC_TYPE = "vc+sd-jwt"
 
@@ -56,83 +51,25 @@ const val SD_JWT_VC_TYPE = "vc+sd-jwt"
  * **Note:** The optional dependency 'com.google.crypto.tink:tink' is required when support for [OctetKeyPair] is required.
  */
 internal class SdJwtVcJwtProcessor<C : SecurityContext>(
-    private val jwkSource: JWKSource<C>,
-) : DefaultJWTProcessor<C>() {
-    init {
-        jwsTypeVerifier = DefaultJOSEObjectTypeVerifier(JOSEObjectType(SD_JWT_VC_TYPE))
-        jwtClaimsSetVerifier = DefaultJWTClaimsVerifier(
+    jwkSource: JWKSource<C>,
+) : JwkSourceJWTProcessor<C>(typeVerifier(), claimSetVerifier(), jwkSource) {
+
+    companion object {
+        private fun <C : SecurityContext> typeVerifier(): JOSEObjectTypeVerifier<C> =
+            DefaultJOSEObjectTypeVerifier(JOSEObjectType(SD_JWT_VC_TYPE))
+
+        private fun <C : SecurityContext> claimSetVerifier(): JWTClaimsSetVerifier<C> = DefaultJWTClaimsVerifier(
             JWTClaimsSet.Builder().build(),
             setOf("iss"),
         )
-    }
-
-    override fun process(signedJWT: SignedJWT, context: C?): JWTClaimsSet {
-        ensureInitialized()
-
-        jwsTypeVerifier.verify(signedJWT.header.type, context)
-
-        val claimsSet = signedJWT.jwtClaimSet()
-        val jwkSelector = JWKSelector(JWKMatcher.forJWSHeader(signedJWT.header))
-
-        val jwks = jwkSource.get(jwkSelector, context)
-        if (jwks.isNullOrEmpty()) {
-            throw BadJOSEException("Signed JWT rejected: Another algorithm expected, or no matching key(s) found")
-        }
-
-        for (jwk in jwks) {
-            val verifier = jwsVerifierFor(signedJWT.header.algorithm, jwk)
-            if (signedJWT.verify(verifier)) {
-                jwtClaimsSetVerifier.verify(claimsSet, context)
-                return claimsSet
-            }
-        }
-
-        // No more keys to try out
-        throw BadJOSEException("Signed JWT rejected: Invalid signature or no matching verifier(s) found")
-    }
-
-    private fun ensureInitialized() {
-        if (jwsTypeVerifier == null) {
-            throw BadJOSEException("Signed JWT rejected: No JWS header typ (type) verifier is configured")
-        }
-
-        if (jwtClaimsSetVerifier == null) {
-            throw BadJOSEException("Signed JWT rejected: No JWTClaimSet verifier is configured")
-        }
-    }
-
-    companion object {
 
         /**
          * Gets a [JWKSource] for a DID Document.
          */
-        fun <C : SecurityContext> didJwkSet(jwsHeader: JWSHeader, jwkSet: JWKSet): JWKSource<C> = DIDJWKSet<C>(jwsHeader, jwkSet)
+        fun <C : SecurityContext> didJwkSet(jwsHeader: JWSHeader, jwkSet: JWKSet): JWKSource<C> =
+            DIDJWKSet<C>(jwsHeader, jwkSet)
     }
 }
-
-private fun SignedJWT.jwtClaimSet(): JWTClaimsSet =
-    try {
-        getJWTClaimsSet()
-    } catch (e: ParseException) {
-        // Payload not a JSON object
-        throw BadJWTException(e.message, e)
-    }
-
-private fun jwsVerifierFor(algorithm: JWSAlgorithm, jwk: JWK): JWSVerifier =
-    when (algorithm) {
-        in JWSAlgorithm.Family.HMAC_SHA -> MACVerifier(jwk.expectIs<OctetSequenceKey>())
-        in JWSAlgorithm.Family.RSA -> RSASSAVerifier(jwk.expectIs<RSAKey>())
-        in JWSAlgorithm.Family.EC -> ECDSAVerifier(jwk.expectIs<ECKey>())
-        in JWSAlgorithm.Family.ED -> Ed25519Verifier(jwk.expectIs<OctetKeyPair>())
-        else -> throw BadJOSEException("Unsupported JWS algorithm $algorithm")
-    }
-
-private inline fun <reified T> JWK.expectIs(): T =
-    if (this is T) {
-        this
-    } else {
-        throw BadJOSEException("Expected a JWK of type ${T::class.java.simpleName}")
-    }
 
 /**
  * [JWKSource] implementation for DID Documents.
@@ -153,6 +90,7 @@ private class DIDJWKSet<C : SecurityContext>(jwsHeader: JWSHeader, val jwkSet: J
                     .algorithms(algorithm, null)
                     .x509CertSHA256Thumbprint(jwsHeader.x509CertSHA256Thumbprint)
                     .build()
+
             in JWSAlgorithm.Family.ED ->
                 JWKMatcher.Builder()
                     .keyType(KeyType.forAlgorithm(algorithm))
@@ -160,6 +98,7 @@ private class DIDJWKSet<C : SecurityContext>(jwsHeader: JWSHeader, val jwkSet: J
                     .algorithms(algorithm, null)
                     .curves(Curve.forJWSAlgorithm(algorithm))
                     .build()
+
             else -> error("Unsupported JWSAlgorithm '$algorithm'")
         }
         JWKSelector(matcher)
