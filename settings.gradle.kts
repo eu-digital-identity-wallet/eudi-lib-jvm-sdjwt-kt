2afbc48528d9d5dbcc9f5675e9850c929a24bb46
babisRoutis <haralampos.routis@netcompany.com>
2025-04-09 22:52:55 +0300

    Removed redundant plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,27 @@
+
+pluginManagement {
+    repositories {
+        mavenLocal()
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    //
+    // Provides a repository for downloading JVMs
+    //
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+}
+
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
 rootProject.name = "eudi-lib-jvm-sdjwt-kt"
