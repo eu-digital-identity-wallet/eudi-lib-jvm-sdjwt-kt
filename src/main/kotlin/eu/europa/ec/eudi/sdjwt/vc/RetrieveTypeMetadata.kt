a709a7301d8976490c5cc12fd6478d195d35d82f
Anastasios Tsantoulis <anastasios.tsantoulis@netcompany.com>
2025-05-21 14:09:53 +0300

    Add RetrieveTypeMetadata with default impl (#348)
    ---------
    
    Signed-off-by: Anastasios Tsantoulis <Anastasios.TSANTOULIS@netcompany.com>

---
+++ src/main/kotlin/eu/europa/ec/eudi/sdjwt/vc/RetrieveTypeMetadata.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2023 European Commission
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.europa.ec.eudi.sdjwt.vc
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+
+fun interface RetrieveTypeMetadata {
+    suspend operator fun invoke(vct: Vct): Result<SdJwtVcTypeMetadata>
+}
+
+class RetrieveTypeMetadataUsingKtor(
+    private val httpClientFactory: KtorHttpClientFactory = DefaultHttpClientFactory,
+) : RetrieveTypeMetadata {
+    override suspend fun invoke(vct: Vct): Result<SdJwtVcTypeMetadata> = runCatching {
+        val url = Url(vct.value)
+        require(url.protocol == URLProtocol.HTTPS)
+        httpClientFactory().use { httpClient -> httpClient.retrieveTypeMetadata(url) }
+    }
+
+    private suspend fun HttpClient.retrieveTypeMetadata(url: Url): SdJwtVcTypeMetadata {
+        val httpResponse = get(url) {
+            url { URLProtocol.HTTPS }
+            headers { append(HttpHeaders.Accept, ContentType.Application.Json) }
+        }
+        check(httpResponse.status.isSuccess(), { "Failed to retrieve type metadata from $url: ${httpResponse.status.description}" })
+        return httpResponse.body<SdJwtVcTypeMetadata>()
+    }
+}
