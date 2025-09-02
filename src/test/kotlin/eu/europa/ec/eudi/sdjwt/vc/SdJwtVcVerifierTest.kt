ec61db7a02d58c18ebe96422f95e9a3f07dafe79
Dimitris Zarras <138439389+dzarras@users.noreply.github.com>
2025-09-02 10:00:03 +0300

    Remove HttpClientFactory (#392)

--- src/test/kotlin/eu/europa/ec/eudi/sdjwt/vc/SdJwtVcVerifierTest.kt
+++ src/test/kotlin/eu/europa/ec/eudi/sdjwt/vc/SdJwtVcVerifierTest.kt
@@ -151,9 +151,7 @@ class SdJwtVcVerifierTest {
     fun `SdJwtVcVerifier should verify an SD-JWT-VC when iss is HTTPS url using kid`() = runTest {
         val unverifiedSdJwt = SampleIssuer.issueUsingKid(kid = SampleIssuer.KEY_ID)
         val verifier = DefaultSdJwtOps.SdJwtVcVerifier(
-            IssuerVerificationMethod.usingIssuerMetadata {
-                HttpMock.clientReturning(SampleIssuer.issuerMeta)
-            },
+            IssuerVerificationMethod.usingIssuerMetadata(HttpMock.clientReturning(SampleIssuer.issuerMeta)),
             TypeMetadataPolicy.NotUsed,
         )
         verifier.verify(unverifiedSdJwt).getOrThrow()
@@ -163,9 +161,7 @@ class SdJwtVcVerifierTest {
     fun `SdJwtVcVerifier should verify an SD-JWT-VC when iss is HTTPS url and no kid`() = runTest {
         val unverifiedSdJwt = SampleIssuer.issueUsingKid(kid = null)
         val verifier = DefaultSdJwtOps.SdJwtVcVerifier(
-            IssuerVerificationMethod.usingIssuerMetadata {
-                HttpMock.clientReturning(SampleIssuer.issuerMeta)
-            },
+            IssuerVerificationMethod.usingIssuerMetadata(HttpMock.clientReturning(SampleIssuer.issuerMeta)),
             TypeMetadataPolicy.NotUsed,
         )
         verifier.verify(unverifiedSdJwt).getOrThrow()
@@ -176,9 +172,7 @@ class SdJwtVcVerifierTest {
         // In case the issuer uses the KID
         val unverifiedSdJwt = SampleIssuer.issueUsingKid("wrong kid")
         val verifier = DefaultSdJwtOps.SdJwtVcVerifier(
-            IssuerVerificationMethod.usingIssuerMetadata {
-                HttpMock.clientReturning(SampleIssuer.issuerMeta)
-            },
+            IssuerVerificationMethod.usingIssuerMetadata(HttpMock.clientReturning(SampleIssuer.issuerMeta)),
             TypeMetadataPolicy.NotUsed,
         )
         try {
