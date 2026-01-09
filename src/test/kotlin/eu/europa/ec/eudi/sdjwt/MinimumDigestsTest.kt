53d5a4ebe85b41659f71f9b1867716f2cb7ff863
babisRoutis <haralampos.routis@netcompany.com>
2026-01-09 21:58:22 +0200

    Some more test optimizations

--- src/test/kotlin/eu/europa/ec/eudi/sdjwt/MinimumDigestsTest.kt
+++ src/test/kotlin/eu/europa/ec/eudi/sdjwt/MinimumDigestsTest.kt
@@ -52,14 +52,8 @@ class MinimumDigestsTest {
         }
 
     private val key = ECKeyGenerator(Curve.P_521).generate()
-    private val issuer: SdJwtIssuer<SignedJWT> =
-        NimbusSdJwtOps.issuer(
-            signer = ECDSASigner(key),
-            signAlgorithm = JWSAlgorithm.ES512,
-            sdJwtFactory = SdJwtFactory(fallbackMinimumDigests = MinimumDigests(4)),
-        )
 
-    fun issuerWithImplicitMinimumDigests(minimumDigests: Int?): SdJwtIssuer<SignedJWT> =
+    fun issuerWithFallbackMinimumDigests(minimumDigests: Int?): SdJwtIssuer<SignedJWT> =
         NimbusSdJwtOps.issuer(
             signer = ECDSASigner(key),
             signAlgorithm = JWSAlgorithm.ES512,
@@ -73,97 +67,36 @@ class MinimumDigestsTest {
         assertTrue("Expecting at least $minimumDigests found $digestsNo") { minimumDigests <= digestsNo }
     }
 
-    private fun testMinimumDigestsAtTopLevel(
+    private suspend fun testMinimumDigestsAtTopLevel(
         issuer: SdJwtIssuer<SignedJWT>,
         minimumDigests: Int,
-    ): suspend (SdJwtObject) -> SdJwt<SignedJWT> = { spec ->
+        spec: SdJwtObject,
+    ): SdJwt<SignedJWT> {
         val sdJwt = issuer.issue(spec).getOrThrow()
         sdJwt.prettyPrint { it.jwtClaimsSet.jsonObject() }
         val digests = sdJwt.topLevelDigests()
         assertMinimumDigests(minimumDigests, digests.size)
-        sdJwt
+        return sdJwt
     }
 
-    @Test
-    fun checkExplicitMinimumDigestsAtTopLevel() = runTest {
-        val minimumDigests = 3
-        val test = testMinimumDigestsAtTopLevel(issuer, minimumDigests)
-        listOf(
-            sdJwt(minimumDigests) {
-                sdClaim("foo", "bar")
-            },
-
-            sdJwt(minimumDigests) {
-                for (i in 0..<minimumDigests + 1)
-                    sdClaim("$i", "value of $i")
-            },
-            sdJwt(minimumDigests) {
-                sdArrClaim("baz") {
-                    sdClaim("qux")
-                }
-            },
-        ).forEach { test(it) }
-    }
-
-    @Test
-    fun checkImplicitMinimumDigestsAtTopLevel() = runTest {
-        val implicitMinimumDigests = 3
-        val issuer = issuerWithImplicitMinimumDigests(implicitMinimumDigests)
-        val test = testMinimumDigestsAtTopLevel(issuer, implicitMinimumDigests)
-
-        listOf(
-            sdJwt {
-                sdClaim("foo", "bar")
-            },
-
-            sdJwt {
-                for (i in 0..<implicitMinimumDigests + 1)
-                    sdClaim("$i", "value of $i")
-            },
-            sdJwt {
-                sdArrClaim("baz") {
-                    sdClaim("qux")
-                }
-            },
-        ).forEach { test(it) }
-    }
-
-    private suspend fun checkMinimumDigestsForNestedElement(
+    private suspend fun checkDigestsForNestedElement(
         spec: SdJwtObject,
         elementName: String,
-        implicitMinimumDigests: Int?,
+        fallbackMinimumDigests: Int?,
         nestedElementMinimumDigests: Int?,
     ) {
-        val issuer = issuerWithImplicitMinimumDigests(implicitMinimumDigests)
-        val expectedTopLevelDigests = implicitMinimumDigests ?: 0
-        val sdJwt = testMinimumDigestsAtTopLevel(issuer, expectedTopLevelDigests)(spec)
+        val issuer = issuerWithFallbackMinimumDigests(fallbackMinimumDigests)
+        val expectedTopLevelDigests = fallbackMinimumDigests ?: 0
+        val sdJwt = testMinimumDigestsAtTopLevel(issuer, expectedTopLevelDigests, spec)
         val digests = sdJwt.digestsOf(elementName)
         val expectedNestedDigests = nestedElementMinimumDigests ?: expectedTopLevelDigests
         assertMinimumDigests(expectedNestedDigests, digests.size)
     }
 
     @Test
-    fun checkExplicitMinimumDigestsAtNestedSdObj() = runTest {
-        val nestedElementMinimumDigests = 6
-        val spec = sdJwt {
-            sdObjClaim("someObj", nestedElementMinimumDigests) {
-                sdClaim("foo", "bar")
-                sdClaim("baz", "qux")
-                sdClaim("quux", "corge")
-            }
-        }
-        checkMinimumDigestsForNestedElement(
-            spec = spec,
-            elementName = "someObj",
-            implicitMinimumDigests = null,
-            nestedElementMinimumDigests = nestedElementMinimumDigests,
-        )
-    }
-
-    @Test
-    fun checkImplicitMinimumDigestsAtNestedSdObj() = runTest {
+    fun checkFallbackMinimumDigestsAtNestedSdObj() = runTest {
         // This spec contains a nested object with no explicitly defined minimum digests
-        // Implicitly defined minimum digest should appear
+        // fallback defined minimum digest should appear
         val spec = sdJwt {
             sdObjClaim("someObj") {
                 sdClaim("foo", "bar")
@@ -171,18 +104,18 @@ class MinimumDigestsTest {
                 sdClaim("quux", "corge")
             }
         }
-        checkMinimumDigestsForNestedElement(
+        checkDigestsForNestedElement(
             spec = spec,
             elementName = "someObj",
-            implicitMinimumDigests = 6,
+            fallbackMinimumDigests = 6,
             nestedElementMinimumDigests = null,
         )
     }
 
     @Test
-    fun checkImplicitMinimumDigestsAndExplicitMinimumDigestsAtNestedSdObj() = runTest {
+    fun checkExplicitMinimumDigestsAtNestedSdObj() = runTest {
         // This spec contains a nested object claim which explicitly defines minimum digests
-        // implicit mimimum digests should be ignored, for the object
+        // fallback minimum digests should be ignored, for the object
         val nestedElementMinimumDigests = 6
         val spec = sdJwt {
             sdObjClaim("someObj", nestedElementMinimumDigests) {
@@ -191,10 +124,10 @@ class MinimumDigestsTest {
                 sdClaim("quux", "corge")
             }
         }
-        checkMinimumDigestsForNestedElement(
+        checkDigestsForNestedElement(
             spec = spec,
             elementName = "someObj",
-            implicitMinimumDigests = nestedElementMinimumDigests + 1,
+            fallbackMinimumDigests = nestedElementMinimumDigests + 1,
             nestedElementMinimumDigests = nestedElementMinimumDigests,
         )
     }
@@ -202,7 +135,7 @@ class MinimumDigestsTest {
     @Test
     fun checkExplicitMinimumDigestsAtNestedSdArray() = runTest {
         // This spec contains a nested array claim which explicitly defines minimum digests
-        // implicit mimimum digests should be ignored, for the array
+        // fallback minimum digests should be ignored, for the array
         val nestedElementMinimumDigests = 6
         val spec = sdJwt {
             sdArrClaim("someArr", nestedElementMinimumDigests) {
@@ -212,16 +145,16 @@ class MinimumDigestsTest {
                 sdClaim("baz")
             }
         }
-        checkMinimumDigestsForNestedElement(
+        checkDigestsForNestedElement(
             spec = spec,
             elementName = "someArr",
-            implicitMinimumDigests = nestedElementMinimumDigests + 2,
+            fallbackMinimumDigests = nestedElementMinimumDigests + 2,
             nestedElementMinimumDigests = nestedElementMinimumDigests,
         )
     }
 
     @Test
-    fun checkImplicitMinimumDigestsAtNestedSdArray() = runTest {
+    fun checkFallbackMinimumDigestsAtNestedSdArray() = runTest {
         // This spec contains a nested array claim which doesn't explicitly define minimum digests
         // it should inherit the implicit minimum digests from the parent SD-JWT
         val spec = sdJwt {
@@ -232,10 +165,10 @@ class MinimumDigestsTest {
                 sdClaim("baz")
             }
         }
-        checkMinimumDigestsForNestedElement(
+        checkDigestsForNestedElement(
             spec = spec,
             elementName = "someArr",
-            implicitMinimumDigests = 10,
+            fallbackMinimumDigests = 10,
             nestedElementMinimumDigests = null,
         )
     }
