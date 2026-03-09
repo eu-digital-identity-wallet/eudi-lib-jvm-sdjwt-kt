ff28612f0f763b4af828d2f6cbdb6673590c5fe1
Anastasios Tsantoulis <anastasios.tsantoulis@netcompany.com>
2026-03-09 17:33:12 +0200

    SD-JWT verification: Missing Validity Checks for claims ‘nbf’, and ‘exp’. (#456)
    ---------
    
    Signed-off-by: Anastasios Tsantoulis <anastasios.tsantoulis@netcompany.com>
    Co-authored-by: Dimitris ZARRAS <Dimitris.ZARRAS@netcompany.com>

--- src/test/kotlin/eu/europa/ec/eudi/sdjwt/examples/ExamplesDataAndUtils.kt
+++ src/test/kotlin/eu/europa/ec/eudi/sdjwt/examples/ExamplesDataAndUtils.kt
@@ -16,14 +16,18 @@
 package eu.europa.ec.eudi.sdjwt.examples
 
 import com.nimbusds.jose.JWSAlgorithm
+import com.nimbusds.jose.crypto.RSASSASigner
 import com.nimbusds.jose.jwk.Curve
 import com.nimbusds.jose.jwk.ECKey
 import com.nimbusds.jose.jwk.RSAKey
 import com.nimbusds.jose.jwk.gen.ECKeyGenerator
 import com.nimbusds.jose.util.Base64
 import com.nimbusds.jose.util.X509CertUtils
+import eu.europa.ec.eudi.sdjwt.NimbusSdJwtOps
+import eu.europa.ec.eudi.sdjwt.dsl.values.sdJwt
 import eu.europa.ec.eudi.sdjwt.loadRsaKey
 import io.ktor.http.*
+import kotlinx.coroutines.runBlocking
 import org.bouncycastle.asn1.DERSequence
 import org.bouncycastle.asn1.x509.Extension
 import org.bouncycastle.asn1.x509.GeneralName
@@ -45,6 +49,54 @@ internal val issuerEcKeyPair: ECKey by lazy {
 }
 
 internal val issuerRsaKeyPair: RSAKey by lazy { loadRsaKey("/examplesIssuerKey.json") }
+
+internal val serializedUnverifiedPresentationSdJwt: String = runBlocking {
+    val issuer = with(NimbusSdJwtOps) {
+        issuer(signer = RSASSASigner(issuerRsaKeyPair), signAlgorithm = JWSAlgorithm.RS256)
+    }
+
+    val spec = sdJwt {
+        claim("sub", "6c5c0a49-b589-431d-bae7-219122a9ec2c")
+        objClaim("address") {
+            sdClaim("locality", "Schulpforta")
+            sdClaim("region", "Sachsen-Anhalt")
+        }
+        claim("iss", "https://example.com/issuer")
+        val now = Clock.System.now()
+        claim("iat", now.epochSeconds)
+        claim("exp", (now + 31.days).epochSeconds)
+    }
+
+    val sdJwt = issuer.issue(spec).getOrThrow()
+    with(NimbusSdJwtOps) {
+        sdJwt.serialize()
+    }
+}
+
+internal val serializedUnverifiedIssuanceSdJwt: String = runBlocking {
+    val issuer = with(NimbusSdJwtOps) {
+        issuer(signer = RSASSASigner(issuerRsaKeyPair), signAlgorithm = JWSAlgorithm.RS256)
+    }
+
+    val spec = sdJwt {
+        claim("sub", "6c5c0a49-b589-431d-bae7-219122a9ec2c")
+        objClaim("address") {
+            sdClaim("locality", "Schulpforta")
+            sdClaim("region", "Sachsen-Anhalt")
+            sdClaim("region", "Sachsen-Anhalt")
+            sdClaim("country", "DE")
+        }
+        claim("iss", "https://example.com/issuer")
+        val now = Clock.System.now()
+        claim("iat", now.epochSeconds)
+        claim("exp", (now + 31.days).epochSeconds)
+    }
+
+    val sdJwt = issuer.issue(spec).getOrThrow()
+    with(NimbusSdJwtOps) {
+        sdJwt.serialize()
+    }
+}
 
 internal val issuerEcKeyPairWithCertificate: ECKey by lazy {
     val issuer = Url("https://issuer.example.com")
